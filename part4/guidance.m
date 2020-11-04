f94e0a9ac60b130e23dd87e7da4bcd741161c151
Eirik! <eirikjda@ntnu.stud.no>
2020-11-04 15:31:56 +0100

    boom

--- part4/guidance.m
+++ part4/guidance.m
@@ -1,9 +1,11 @@
 function [ chi_d ] = guidance(x_t, y_t, x_ref, y_ref, x, y,L)
     pi_p = atan2(y_t-y_ref,x_t-x_ref);
     %xhi_inf = pi/2;
-    [x_p, y_p, y_e] = crosstrack(x_t, y_t, x_ref, y_ref, x, y);
-    K_p = 200; %2*L
+    [y_e] = crosstrackWpt(x_t, y_t, x_ref, y_ref, x, y);
+    delta = 800;
+    K_p = 1/delta; %2*L
     chi_d = pi_p - atan(K_p*y_e);
-    chi_d = ssa(chi_d);
+    chi_d = wrapTo2Pi(chi_d);
+    %chi_d = ssa(chi_d);
 end
 
