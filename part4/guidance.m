84aeb6d7e75f3c13efa65a7529bdd070ccf96e7b
Eirik! <eirikjda@ntnu.stud.no>
2020-11-04 15:00:47 +0100

    guidance

--- part4/guidance.m
+++ part4/guidance.m
@@ -1,7 +1,9 @@
-function [ chi_d ] = guidance(x_t, y_t, x_ref, y_ref, x, y)
+function [ chi_d ] = guidance(x_t, y_t, x_ref, y_ref, x, y,L)
     pi_p = atan2(y_t-y_ref,x_t-x_ref);
     %xhi_inf = pi/2;
     [x_p, y_p, y_e] = crosstrack(x_t, y_t, x_ref, y_ref, x, y);
-    chi_d = pi_p - arctan(K_p_wp*y_e);
+    K_p = 200; %2*L
+    chi_d = pi_p - arctan(K_p*y_e);
+    chi_d = ssa(chi_d);
 end
 
